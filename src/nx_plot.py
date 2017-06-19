9cc05a475adbdb35937e93f1867bb6fa6bf0828f
ayou7995 <ayou7995@gmail.com>
2017-06-19 22:33:40 +0800

    rearange directory

--- src/nx_plot.py
+++ src/nx_plot.py
@@ -11,7 +11,9 @@ global Rfilter
 global Cfilter
 global Mfilter
 
-proDir = '../procedure/'
+basDir = '../basic/procedure/'
+optDir = '../optimize/procedure/'
+nonDir = '../nonuniform/procedure/'
 figDir = '../figure/'
 
 colors = ['black','brown','red','orange','yellow','green','blue','purple','gray','white','pink']
@@ -26,7 +28,7 @@ Mfilter = lambda obj: any(obj['M']==m for m in [1])
 
 # def newDict():
 
-def plot(objs, title, filename, topSize=3/5, includeOrigin=True):
+def plotBasic(objs, title, filename, topSize=3/5, includeOrigin=True):
     plt.figure()
     for i in range(len(objs)):
         _hist = objs[i]['hist'] / objs[i]['hist'].size 
@@ -52,20 +54,35 @@ def plot(objs, title, filename, topSize=3/5, includeOrigin=True):
     plt.legend()
     plt.savefig(join(figDir,filename))
 
-def parse(paths, cost=False):
+def parse(paths, types):
     data = []
-    for path in paths:
-        obj = {}
-        token = path.replace(proDir,'').replace('.npy','').split('_')
-        obj['N'] = token[0]
-        obj['T'] = token[1][1]
-        obj['K'] = int(token[2][1:])
-        obj['R'] = float(token[3][1:])
-        obj['C'] = token[4][1]
-        obj['M'] = int(token[5][1:])
-        obj['hist'] = np.load(path)
-        obj['thres'] = threshold(obj['hist'], [0.5, 0.25, 0.1, 0.01, 0.001])
-        data.append(obj)
+    if types == 'basic' or types == 'optimize':
+        for path in paths:
+            obj = {}
+            token = path.replace(basDir,'').replace('.npy','').split('_')
+            obj['N'] = token[0]
+            obj['T'] = token[1][1]
+            obj['K'] = int(token[2][1:])
+            obj['R'] = float(token[3][1:])
+            obj['C'] = token[4][1]
+            obj['M'] = int(token[5][1:])
+            if types == 'optimize':
+                obj['cost'] = int(token[6][1:])
+            obj['hist'] = np.load(path)
+            obj['thres'] = threshold(obj['hist'], [0.5, 0.25, 0.1, 0.01, 0.001])
+            data.append(obj)
+    elif types == 'nonuniform':
+        for path in paths:
+            obj = {}
+            token = path.replace(basDir,'').replace('.npy','').split('_')
+            obj['N'] = token[0]
+            obj['T'] = token[1][1]
+            obj['D'] = int(token[2][1:])
+            obj['M'] = int(token[3][1:])
+            obj['cost'] = int(token[4][1:])
+            obj['hist'] = np.load(path)
+            obj['thres'] = threshold(obj['hist'], [0.5, 0.25, 0.1, 0.01, 0.001])
+            data.append(obj)
     return data
 
 def setFilter(nL, tL, kL, rL, cL, mL):
@@ -87,11 +104,9 @@ def sifter(objlist):
     return list(filter(lambda x: all(f(x) for f in filters), objlist))
 
 def threshold(hist, thres):
-    # hist = np.flip(hist,0)
-    # wholeNum = hist
+    j = 0
     hist = hist / hist.size
     temp = np.array([0. for i in range(len(thres))])
-    j = 0
     for i in reversed(range(len(hist))):
         if hist[i] < thres[j]:
             temp[j] = hist[i]
@@ -102,89 +117,89 @@ def threshold(hist, thres):
 
 if __name__ == '__main__':
 
-    origin = np.load(join(proDir,'as06_origin.npy'))
+    # origin percolation data
+    origin = np.load(join(basDir,'as06_origin.npy'))
     origin_thres = threshold(origin, [0.5, 0.25, 0.1, 0.01, 0.001])
-    print('origin_thres')
-    print(origin_thres)
-    plot([], 'origin', 'as06_origin')
+    plotBasic([], 'origin', 'as06_origin')
 
-    paths = [p for p in glob.glob(join(proDir,'*.npy')) if 'origin' not in p]
-    objlist = parse(paths)
+    # basiclist  
+    paths = [p for p in glob.glob(join(basDir,'*.npy')) if 'origin' not in p]
+    objlist = parse(paths, 'basic')
 
     #####################################################################
     # as06_Tx_K3_R0.02_Cd_M1
     setFilter(['as06'],['c','s','t','r'],[3],[0.02],['d'],[1])
     data = sorted(sifter(objlist), key=lambda obj: obj['T'])
-    plot(data, 'as06 - topology', 'as06_Tx_K3_R0.02_Cd_M1.png', 4/5)
+    plotBasic(data, 'as06 - topology', 'as06_Tx_K3_R0.02_Cd_M1.png', 4/5)
     
     # as06_Tx_K4_R0.02_Cd_M1
     setFilter(['as06'],['c','s','t','r'],[4],[0.02],['d'],[1])
     data = sorted(sifter(objlist), key=lambda obj: obj['T'])
-    plot(data, 'as06 - topology', 'as06_Tx_K4_R0.02_Cd_M1.png', 4/5)
+    plotBasic(data, 'as06 - topology', 'as06_Tx_K4_R0.02_Cd_M1.png', 4/5)
     
     # as06_Tx_K5_R0.02_Cd_M1
     setFilter(['as06'],['c','s','t','r'],[5],[0.02],['d'],[1])
     data = sorted(sifter(objlist), key=lambda obj: obj['T'])
-    plot(data, 'as06 - topology', 'as06_Tx_K5_R0.02_Cd_M1.png', 4/5)
+    plotBasic(data, 'as06 - topology', 'as06_Tx_K5_R0.02_Cd_M1.png', 4/5)
     
     # as06_Tx_K6_R0.02_Cd_M1
     setFilter(['as06'],['c','s','t','r'],[6],[0.02],['d'],[1])
     data = sorted(sifter(objlist), key=lambda obj: obj['T'])
-    plot(data, 'as06 - topology', 'as06_Tx_K6_R0.02_Cd_M1.png', 4/5)
+    plotBasic(data, 'as06 - topology', 'as06_Tx_K6_R0.02_Cd_M1.png', 4/5)
 
     ##########################################################################
     # as06_Tc_Kx_R0.005_Cd_M1
     setFilter(['as06'],['c'],[2,3,4,5,6],[0.005],['d'],[1])
     data = sorted(sifter(objlist), key=lambda obj: obj['K'])
-    plot(data, 'as06 - # of Split Nodes', 'as06_Tc_Kx_R0.005_Cd_M1.png', 4/5)
+    plotBasic(data, 'as06 - # of Split Nodes', 'as06_Tc_Kx_R0.005_Cd_M1.png', 4/5)
 
     # as06_Tc_Kx_R0.01_Cd_M1
     setFilter(['as06'],['c'],[2,3,4,5,6],[0.01],['d'],[1])
     data = sorted(sifter(objlist), key=lambda obj: obj['K'])
-    plot(data, 'as06 - # of Split Nodes', 'as06_Tc_Kx_R0.01_Cd_M1.png', 4/5)
+    plotBasic(data, 'as06 - # of Split Nodes', 'as06_Tc_Kx_R0.01_Cd_M1.png', 4/5)
 
     # as06_Tc_Kx_R0.02_Cd_M1
     setFilter(['as06'],['c'],[2,3,4,5,6],[0.02],['d'],[1])
     data = sorted(sifter(objlist), key=lambda obj: obj['K'])
-    plot(data, 'as06 - # of Split Nodes', 'as06_Tc_Kx_R0.02_Cd_M1.png', 4/5)
+    plotBasic(data, 'as06 - # of Split Nodes', 'as06_Tc_Kx_R0.02_Cd_M1.png', 4/5)
 
     # as06_Tc_Kx_R0.04_Cd_M1
     setFilter(['as06'],['c'],[2,3,4,5,6],[0.04],['d'],[1])
     data = sorted(sifter(objlist), key=lambda obj: obj['K'])
-    plot(data, 'as06 - # of Split Nodes', 'as06_Tc_Kx_R0.04_Cd_M1.png', 4/5)
+    plotBasic(data, 'as06 - # of Split Nodes', 'as06_Tc_Kx_R0.04_Cd_M1.png', 4/5)
 
     # as06_Tc_Kx_R0.08_Cd_M1
     setFilter(['as06'],['c'],[2,3,4,5,6],[0.08],['d'],[1])
     data = sorted(sifter(objlist), key=lambda obj: obj['K'])
-    plot(data, 'as06 - # of Split Nodes', 'as06_Tc_Kx_R0.08_Cd_M1.png', 4/5)
+    plotBasic(data, 'as06 - # of Split Nodes', 'as06_Tc_Kx_R0.08_Cd_M1.png', 4/5)
 
     # as06_Tc_Kx_R0.16_Cd_M1
     setFilter(['as06'],['c'],[2,3,4,5,6],[0.16],['d'],[1])
     data = sorted(sifter(objlist), key=lambda obj: obj['K'])
-    plot(data, 'as06 - # of Split Nodes', 'as06_Tc_Kx_R0.16_Cd_M1.png', 4/5)
+    plotBasic(data, 'as06 - # of Split Nodes', 'as06_Tc_Kx_R0.16_Cd_M1.png', 4/5)
 
     ###########################################################################
     # as06_Tc_K2_R0.02_Cd_Mx
     setFilter(['as06'],['c'],[2],[0.02],['d'],[1,2])
     data = sorted(sifter(objlist), key=lambda obj: obj['M'])
-    plot(data, 'as06 - replicate', 'as06_Tc_K2_R0.02_Cd_Mx.png', 3/5)
+    plotBasic(data, 'as06 - replicate', 'as06_Tc_K2_R0.02_Cd_Mx.png', 3/5)
 
     # as06_Tc_K3_R0.02_Cd_Mx
     setFilter(['as06'],['c'],[3],[0.02],['d'],[1,2,3])
     data = sorted(sifter(objlist), key=lambda obj: obj['M'])
-    plot(data, 'as06 - replicate', 'as06_Tc_K3_R0.02_Cd_Mx.png', 3/5)
+    plotBasic(data, 'as06 - replicate', 'as06_Tc_K3_R0.02_Cd_Mx.png', 3/5)
 
     # as06_Tc_K4_R0.02_Cd_Mx
     setFilter(['as06'],['c'],[4],[0.02],['d'],[1,2,3,4])
     data = sorted(sifter(objlist), key=lambda obj: obj['M'])
-    plot(data, 'as06 - replicate', 'as06_Tc_K4_R0.02_Cd_Mx.png', 3/5)
+    plotBasic(data, 'as06 - replicate', 'as06_Tc_K4_R0.02_Cd_Mx.png', 3/5)
 
     # as06_Tc_K5_R0.02_Cd_Mx
     setFilter(['as06'],['c'],[5],[0.02],['d'],[1,2,3,4,5])
     data = sorted(sifter(objlist), key=lambda obj: obj['M'])
-    plot(data, 'as06 - replicate', 'as06_Tc_K5_R0.02_Cd_Mx.png', 3/5)
+    plotBasic(data, 'as06 - replicate', 'as06_Tc_K5_R0.02_Cd_Mx.png', 3/5)
 
     # as06_Tc_K6_R0.02_Cd_Mx
     setFilter(['as06'],['c'],[6],[0.02],['d'],[1,2,3,4,5,6])
     data = sorted(sifter(objlist), key=lambda obj: obj['M'])
-    plot(data, 'as06 - replicate', 'as06_Tc_K6_R0.02_Cd_Mx.png', 3/5)
+    plotBasic(data, 'as06 - replicate', 'as06_Tc_K6_R0.02_Cd_Mx.png', 3/5)
